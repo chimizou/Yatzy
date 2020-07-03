665e7d968b53d295eafe75e48ac130b484c2cc92
chimizou <hamza.benbouzid@gmail.com>
2020-07-03 18:51:41 +0200

    three / four of a kind

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -1,7 +1,17 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-class DiceHand {
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+
+class DiceHand implements Iterable<Integer> {
     private final int[] dice;
 
     public DiceHand(int d1, int d2, int d3, int d4, int d5) {
@@ -19,6 +29,10 @@ class DiceHand {
         return dice[index];
     }
 
+    @Override
+    public Iterator<Integer> iterator() {
+        return stream().iterator();
+    }
 
     public Stream<Integer> stream() {
         return IntStream.of(dice).boxed();
@@ -26,7 +40,6 @@ class DiceHand {
 }
 
 public class Yatzy {
-
     public static int chance(DiceHand diceHand) {
         return diceHand.stream().mapToInt(Integer::intValue).sum();
     }
@@ -62,66 +75,54 @@ public class Yatzy {
         return diceHand.sumValues(6);
     }
 
-    public static int score_pair(int d1, int d2, int d3, int d4, int d5) {
-        int[] counts = new int[6];
-        counts[d1 - 1]++;
-        counts[d2 - 1]++;
-        counts[d3 - 1]++;
-        counts[d4 - 1]++;
-        counts[d5 - 1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6 - at - 1] >= 2)
-                return (6 - at) * 2;
-        return 0;
+    public static int score_pair(DiceHand diceHand) {
+        Map<Integer, Long> counts = diceHand.stream()
+                .collect(groupingBy(d -> d, counting()));
+        OptionalInt maxDie = counts.entrySet().stream()
+                .filter(e -> e.getValue() >= 2)
+                .mapToInt(Entry::getKey)
+                .max();
+
+        return maxDie.orElse(0) * 2;
     }
 
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5) {
-        int[] counts = new int[6];
-        counts[d1 - 1]++;
-        counts[d2 - 1]++;
-        counts[d3 - 1]++;
-        counts[d4 - 1]++;
-        counts[d5 - 1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6 - i - 1] >= 2) {
-                n++;
-                score += (6 - i);
-            }
-        if (n == 2)
-            return score * 2;
-        else
+    public static int two_pair(DiceHand diceHand) {
+        Map<Integer, Long> counts = diceHand.stream()
+                .collect(groupingBy(d -> d, counting()));
+
+        List<Integer> diceTwoOrMore = counts.entrySet().stream()
+                .filter(e -> e.getValue() >= 2)
+                .map(Entry::getKey)
+                .collect(Collectors.toList());
+
+        if (diceTwoOrMore.size() != 2) {
             return 0;
+        }
+
+        return diceTwoOrMore.stream().mapToInt(Integer::intValue).sum() * 2;
+
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1 - 1]++;
-        tallies[_2 - 1]++;
-        tallies[d3 - 1]++;
-        tallies[d4 - 1]++;
-        tallies[d5 - 1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i + 1) * 4;
-        return 0;
+    public static int three_of_a_kind(DiceHand diceHand) {
+        Map<Integer, Long> counts = diceHand.stream()
+                .collect(groupingBy(d -> d, counting()));
+        OptionalInt maxDie = counts.entrySet().stream()
+                .filter(e -> e.getValue() >= 3)
+                .mapToInt(Entry::getKey)
+                .findAny();
+
+        return maxDie.orElse(0) * 3;
     }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
-        int[] t;
-        t = new int[6];
-        t[d1 - 1]++;
-        t[d2 - 1]++;
-        t[d3 - 1]++;
-        t[d4 - 1]++;
-        t[d5 - 1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i + 1) * 3;
-        return 0;
+    public static int four_of_a_kind(DiceHand diceHand) {
+        Map<Integer, Long> counts = diceHand.stream()
+                .collect(groupingBy(d -> d, counting()));
+        OptionalInt maxDie = counts.entrySet().stream()
+                .filter(e -> e.getValue() >= 4)
+                .mapToInt(Entry::getKey)
+                .findAny();
+
+        return maxDie.orElse(0) * 4;
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
